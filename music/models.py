d5f00e405bf54e6abdbb1a7de46c4d5140347777
Sankalp Shubham <2014UGEC@nitjsr.ac.in>
2016-11-02 01:36:43 +0530

    Music Django App Added

---
+++ music/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+class Album(models.Model):
+    artist = models.CharField(max_length=40)
+    album_title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=30)
+    album_logo = models.CharField(max_length=100)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album,on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=100)
+    song_title = models.CharField(max_length=100)
---
+++ music/urls.py
@@ -0,0 +1,7 @@
+__author__ = 'shubham'
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.index,name="index")
+]
